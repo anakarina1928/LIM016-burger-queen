fa64fc3eff58e147d66169dacaa528eaefe1e7a9
Carolina-Triana <wile1207@gmail.com>
2022-03-02 18:07:40 -0500

    data Form useState

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import {React, useState} from "react"
 import InputForm from "./input.js"
 import Button from "./button.js"
 //import loginWithEmailAndPassword from "../../firebase/auth"
@@ -10,18 +10,36 @@ import Button from "./button.js"
 
 
 function Login () {
-return(<form>
+
+const [data, setData] = useState({
+email: "",
+password: ""
+})
+const onChangeInputs = (e) =>{
+  setData({ ...data, 
+    [e.target.name] : e.target.value
+  })
+}
+const eventButton = (e) =>{
+    e.preventDefault();
+    console.log("datos submit")
+    console.log(data.email, data.password)
+  }
+return(
+<form onSubmit={eventButton}>
     <InputForm
     type="email"
     label="correo electronico"
     placeholder="ingresa correo"
-    name="correo"
+    name="email"
+    onChange={onChangeInputs}
     />
     <InputForm
     type="password"
     label="contraseña"
     placeholder=""
     name="password"
+    onChange={onChangeInputs}
     />
     <Button/>
     </form>
