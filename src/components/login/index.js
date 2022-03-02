daabdbb3171472148c9a89a59e24443258cf34bd
G9D4 <gldt2494@gmail.com>
2022-03-02 14:40:58 -0500

    guarda cambios de formato e identacion

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -2,25 +2,27 @@ import React from "react"
 import InputForm from "./input.js"
 import Button from "./button.js"
 //import loginWithEmailAndPassword from "../../firebase/auth"
-/*const regex = {
-    password: /^.{8,15}$/,
-    email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-  };
+const regex = {
+  password: /^.{8,15}$/,
+  email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+};
 
-  const isEmail = (text) => {
-    return (reg.email.test(text))
-  }
+const isEmail = (text) => {
+  return (reg.email.test(text))
+}
 
-  const isPass = (text) => {
-    return (reg.password.test(text))
-  }
+const isPass = (text) => {
+  return (reg.password.test(text))
+}
 
-  const validateLogin = async () => {
+const validateLogin = async () => {
 
-  }
+}
 
 function Login () {
-return(<form>
+return(
+
+  <form>
     <InputForm
     type="email"
     label="correo electronico"
@@ -34,8 +36,8 @@ return(<form>
     name="password"
     />
     <Button/>
-    </form>
-    )
+  </form>
+  )
 };
 
-export default Login*/
+export default Login
--- src/components/login/input.js
+++ src/components/login/input.js
@@ -1,14 +1,13 @@
 import React from "react"
 
- function InputForm ({name, label, placeholder, type}){
+function InputForm ({name, label, placeholder, type}){
     return (
-    <div>
-    <label htmlFor={name} className="form-label">{label}</label>
-        <input type={type} className="form-input" id={name} placeholder= {placeholder}>
-        </input>
-    </div>
- )}
+        <div>
+        <label htmlFor={name} className="form-label">{label}</label>
+            <input type={type} className="form-input" id={name} placeholder= {placeholder}>
+            </input>
+        </div>
+    )
+}
 
-export default InputForm 
-
-  
+export default InputForm
