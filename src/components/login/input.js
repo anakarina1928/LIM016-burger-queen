fd8791fb2c60e245733153ff93407cb0233f50d6
Carolina-Triana <wile1207@gmail.com>
2022-03-02 13:49:34 -0500

    Component login

---
+++ src/components/login/input.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+ function InputForm ({name, label, placeholder, type}){
+    return (
+    <div>
+    <label htmlFor={name} className="form-label">{label}</label>
+        <input type={type} className="form-input" id={name} placeholder= {placeholder}>
+        </input>
+    </div>
+ )}
+
+export default InputForm 
+
+  
