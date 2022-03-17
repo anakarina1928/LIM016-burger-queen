394cae3b2f7ee68b442f1dec27332771a2084fd7
anakarina1928 <anakarina.nak@gmail.com>
2022-03-16 22:01:29 -0500

    se trabajo en el modal que envia pedido a firebase, se bajaron dependencias y refactorizacion de buutonOrder

--- src/components/waiterView/categoryMenu/buttonOrder.js
+++ src/components/waiterView/categoryMenu/buttonOrder.js
@@ -1,13 +1,37 @@
 import React from "react";
 import './buttonOrder.css'
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-const ButtonOrder = ({ text, onClick  }) => {
+const ButtonOrder = ({productSelect, openModal}) => {
+
+  const confirmOrder = (productSelect) => {
+
+    if (productSelect.length === 0) {
+      toast.warn("¡Orden vacía!", {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        type: "default"
+      });
+
+    } else{
+      openModal();
+    }
+      
+};        
+ 
   return (
     <>
-      <button className="buttonOrder" onClick={onClick} >
-        
-        {text}
+      <button className="buttonOrder" onClick={() => confirmOrder(productSelect)}>
+        enviar
       </button>
+      <ToastContainer />
     </>
   );
 };
