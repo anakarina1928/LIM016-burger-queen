f51cab67573130181dde94faa2edd0d0c9b3e388
anakarina1928 <anakarina.nak@gmail.com>
2022-03-17 19:30:57 -0500

    se trabajo en funcionalidad para obtener  totalidad del prodcuto

--- src/components/nameUser/nameUser.js
+++ src/components/nameUser/nameUser.js
@@ -1,47 +1,46 @@
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import { auth } from "../../firebase/auth.js";
 import { findingUser, collectionUser } from "../../firebase/firestore";
 import "./nameUser.css"
 import { ButtonClose } from "../waiterView/categoryMenu/buttonClose.js";
-import {logOut} from "../../firebase/auth.js";
+import { logOut } from "../../firebase/auth.js";
 
 
 
-export function User(){
-    const [userName, setUserName] = useState("");
-    const [userCargo, setUserCargo] = useState("");
+export function User() {
+  const [userName, setUserName] = useState("");
+  const [userCargo, setUserCargo] = useState("");
 
-      useEffect(() => {    
-        const userId = auth.currentUser.uid;
-        findingUser(userId, collectionUser).then((res) =>
-            setUserName(res.nombre)
-        )
-      }, [])
-
-      useEffect(() => {    
-        const userId = auth.currentUser.uid;
-        findingUser(userId, collectionUser).then((res) =>
-            setUserCargo(res.cargo)
-        )
-      }, [])
-      
-      const logOutSesion=()=>{
-        console.log('a ver');
-        logOut(auth).then(()=>{
-          console.log('log out');
-        }).catch((error) => {
-          console.log(error);
-        });
-        
-      }
-    return(
-        <div className="navUser">
-          <div className="navFlex">
-    <p className="dataUser">{userName}</p>
-    <p className="dataUser">{userCargo}</p>
-    {console.log(auth)}
-    <ButtonClose className="btnLogOut" src={'/icons/stand-by.png'} onClick={logOutSesion}  alt="cerrar sesion" />
-    </div>
-    </div>
+  useEffect(() => {
+    const userId = auth.currentUser.uid;
+    findingUser(userId, collectionUser)
+      .then((res) => setUserName(res.nombre))
+  }, [])
+  
+  useEffect(() => {
+    const userId = auth.currentUser.uid;
+    findingUser(userId, collectionUser).then((res) =>
+      setUserCargo(res.cargo)
     )
+  }, [])
+
+  const logOutSesion = () => {
+    console.log('a ver');
+    logOut(auth).then(() => {
+      console.log('log out');
+    }).catch((error) => {
+      console.log(error);
+    });
+
+  }
+  return (
+    <div className="navUser">
+      <div className="navFlex">
+        <p className="dataUser">{userName}</p>
+        <p className="dataUser">{userCargo}</p>
+        {console.log(auth)}
+        <ButtonClose className="btnLogOut" src={'/icons/stand-by.png'} onClick={logOutSesion} alt="cerrar sesion" />
+      </div>
+    </div>
+  )
 }
