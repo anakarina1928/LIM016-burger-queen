4edcfda9ca3051cee7bebb67915870e66951e029
G9D4 <gldt2494@gmail.com>
2022-03-20 21:50:21 -0500

    full responsive ♥

--- src/components/kitchenView/kitchenMain/kitchenMain.js
+++ src/components/kitchenView/kitchenMain/kitchenMain.js
@@ -1,12 +1,19 @@
 import React from "react";
 import { User } from "../../nameUser/nameUser";
 import { NavKitchen } from "../navKitchen/navKitchen";
+import { OrderList } from "../../orders/orderList";
+import { OrderButtons } from "../../orders/orderButtons";
+import { Ticket } from "../../ticket/ticket";
 
 const KitchenMain = () => {
   return (
     <section className="pendingOrders">
       <User />
       <NavKitchen />
+      <OrderList>
+        <OrderButtons/>
+      </OrderList>
+      <Ticket/>
     </section>
   );
 };
