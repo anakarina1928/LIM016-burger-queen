37438e6eb08a6349d7c03ff05ef5359472bf0d2f
G9D4 <gldt2494@gmail.com>
2022-03-24 09:14:08 -0500

    agrega comentarios

--- src/firebase/firestore.js
+++ src/firebase/firestore.js
@@ -3,50 +3,58 @@ import { collection, doc, getDoc, addDoc, query, onSnapshot, orderBy, where } fr
 import { db } from "./config";
 //import { userDataLocally } from "../api/api";
 
-//const user =userDataLocally();
-//console.log('mesero: ', typeof(user.nombre) );
-//const user2 = '"' + `${user.nombre}` + '"'
 
-//console.log(user2);
-//const userSession = JSON.stringify(user);
-//console.log(userSession, 'que nos trae? ')
-
+//Ref users
 export const collectionUser = collection(db, 'usuarios');
 
+//Ref orders
 const collectionOrder = collection(db, 'order');
 
+//Guarda las ordenes en Firestore
 export const orderToSaveInFirebase = async (newOrder) => {
+
     try {
+
         const savingOrder = await addDoc(collectionOrder, newOrder)
         return savingOrder;
 
     } catch (error) {
+
         console.log('error al guardar pedido del cliente en firebase');
         throw new Error(error);
+
     }
 
 };
 
+//Consigue la data del usuario
 export const findingUser = async (userId, colllection) => {
+
     try {
+
         const documentUserRef = doc(colllection, userId);
         const userDocument = await getDoc(documentUserRef);
         const user = userDocument.data()
-        return user;
-    } catch (error) {
-        throw new Error(error);
-    }
 
+        return user;
+
+    } catch (error) {
+
+        throw new Error(error);
+
+    }
 
 };
 
-//where('worker', '==' , `"${user2}"`),
-//traer data de firebase
+//Trae las ordenes segun su estado en orden descendente del tiempo
 export const onDataOrderChange = (state) => {
+
     return ( (callback) => {
         
         const q = query(collectionOrder, where('state', '==', state), orderBy('init_time', "desc"));
-        onSnapshot(q, callback);
-    })
-};
 
+        onSnapshot(q, callback);
+
+    })
+
+};
